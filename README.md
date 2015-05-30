500b186e97ccbd695785976447b9eea2cf79f336
gbaudry303 <gbaudry303@gmail.com>
2015-05-30 14:26:51 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # office2pdf
 node module to convert office files to pdf
+
+requires unoconv to be installed
+https://github.com/dagwieers/unoconv
+
+convert office files (docx, pptx, xlsx) to an pdf document
+
+### Usage:
+```
+var office2pdf = require(office2pdf),
+  generatePdf = office2pdf.generatePdf;
+
+generatePdf('test/test.pptx', function(err, result) {
+  console.log(result);
+});
---
+++ index.js
@@ -0,0 +1,13 @@
+// Wrapper module unoconv, convert supported office files to pdf
+// unoconv must be present on system
+
+var exec = require('child_process').exec;
+
+module.exports = {
+  generatePdf: function(filename, next) {
+    exec('unoconv -f pdf ' + filename, function (error, stdout, stderr) {
+      if (error) return next(error);
+      return next(null, 'pdf of ' + filename + ' has been successfully generated.' );
+    });
+  }
+};
