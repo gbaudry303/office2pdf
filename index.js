cf2d7fd2b20140d19730a36bc816528115ff240e
gbaudry303 <gbaudry303@gmail.com>
2015-08-13 16:58:50 -0700

    escaping fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var exec = require('child_process').exec;
 
 module.exports = {
   generatePdf: function(filename, next) {
-    exec('unoconv -f pdf ' + filename, function (error, stdout, stderr) {
+    exec('unoconv -f pdf "' + filename +'"', function (error, stdout, stderr) {
       if (error) return next(error);
       return next(null, 'pdf of ' + filename + ' has been successfully generated.' );
     });
